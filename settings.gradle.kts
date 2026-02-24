e94466977f7c26afb81a5240ae6001d40a97389d
AliyahZombie <curaalizm@gmail.com>
2026-02-24 15:52:29 +0000

    fix(build): avoid duplicate version catalog import

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,11 +9,6 @@ dependencyResolutionManagement {
     repositories {
         mavenCentral()
     }
-    versionCatalogs {
-        create("libs") {
-            from(files("gradle/libs.versions.toml"))
-        }
-    }
 }
 
 rootProject.name = "rikkaweb"
